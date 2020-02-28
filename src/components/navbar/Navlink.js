a293dc5a559a0ca7cf1043ca836b0ae1fc4d3524
Gilles <19607616+GillesDP@users.noreply.github.com>
2020-02-28 20:13:24 +0100

    v 0.1.5.2

--- src/components/navbar/Navlink.js
+++ src/components/navbar/Navlink.js
@@ -6,7 +6,7 @@ function Navlink(props) {
 
    return (
       <li className="nav-item">
-         <Link className={`nav-link${props.disabled ? " nav-link--disabled" : ""}`} to={props.link} onClick={props.disabled ? e => e.preventDefault() : ""}>
+         <Link className={`nav-link${props.disabled ? " nav-link--disabled" : ""}`} to={props.link} onClick={props.disabled ? e => e.preventDefault() : null}>
             {props.content}
             {props.content.toLowerCase() === "presets" ? <span className="superscript">SOON</span> : null}
             <span className="arrow d-inline-block d-lg-none">&rarr;</span>
